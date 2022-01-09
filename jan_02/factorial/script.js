d55fd9fea86b5fa369c185788b9c7c409cab451f
Saswat kumar Behera <81437027+theSaswat@users.noreply.github.com>
2022-01-09 10:25:14 +0530

    Update script.js

--- jan_02/factorial/script.js
+++ jan_02/factorial/script.js
@@ -1,1 +1,21 @@
 
+function factorial(num) 
+{ 
+
+  if (num === 0)
+ {
+    return 1;
+ }
+  return num * factorial(num - 1);
+         
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    let x = document.getElementsByTagName("body")[0];
+    x.style.backgroundColor = "#92A8D1";
+    let num = document.getElementById("num");
+    let result = document.getElementById("result");
+    let fact = factorial(num.value);
+    result.innerText = fact;
+})
